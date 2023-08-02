ee7943d0c548f1af327025051b2868f28d87032f
Javier Cruz Santiago <javiercruzsantiago@icloud.com>
2023-08-02 16:53:32 -0600

    [NEW] - Added path drawing to create a Badge

--- SwiftUI/IntroducingSwiftUI/04_DrawingPathsAndShapes/Complete/Landmarks/Views/Helpers/FavoriteButton.swift
+++ SwiftUI/IntroducingSwiftUI/04_DrawingPathsAndShapes/Complete/Landmarks/Views/Helpers/FavoriteButton.swift
@@ -1,13 +1,20 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+A button that acts as a favorites indicator.
+*/
+
 import SwiftUI
 
 struct FavoriteButton: View {
     @Binding var isSet: Bool
-    
+
     var body: some View {
         Button {
             isSet.toggle()
         } label: {
-            Label("Toogle Favorite", systemImage: isSet ? "star.fill" : "star")
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
                 .labelStyle(.iconOnly)
                 .foregroundColor(isSet ? .yellow : .gray)
         }
