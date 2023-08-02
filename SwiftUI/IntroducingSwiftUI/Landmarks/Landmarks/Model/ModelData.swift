936a65652397d25b2486ddb9373cc6c6e5b996f3
Javier Cruz Santiago <javiercruzsantiago@icloud.com>
2023-08-02 12:23:02 -0600

    [NEW] - Added Introducing SwiftUI Landmarks project

---
+++ SwiftUI/IntroducingSwiftUI/Landmarks/Landmarks/Model/ModelData.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+var landmarks: [Landmark] = load("landmarkData.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil) else {
+        fatalError("Couln't find \(filename) in the main bundle.")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couln't load \(filename) from main bundle: \n\(error)")
+    }
+    
+    do {
+        return try JSONDecoder().decode(T.self, from: data)
+    } catch {
+        fatalError("Couln't parse \(filename) as \(T.self): \n\(error)")
+    }
+}
