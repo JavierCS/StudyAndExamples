45294bdb4ea58f9f71476863a1714ea36e32e1a3
Javier Cruz Santiago <javiercruzsantiago@icloud.com>
2023-08-02 20:31:17 -0600

    [NEW]/[SwiftUI] - Added Category Home view to the Landmarks app in order to show it with the Landmark List view in a tab view

--- SwiftUI/IntroducingSwiftUI/06_ComposingComplexInterfaces/Complete/Landmarks/Views/ContentView.swift
+++ SwiftUI/IntroducingSwiftUI/06_ComposingComplexInterfaces/Complete/Landmarks/Views/ContentView.swift
@@ -1,27 +1,34 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+A view showing featured landmarks above a list of all of the landmarks.
+*/
+
 import SwiftUI
 
 struct ContentView: View {
     @State private var selection: Tab = .featured
-    
+
     enum Tab {
         case featured
         case list
     }
-    
+
     var body: some View {
         TabView(selection: $selection) {
             CategoryHome()
-                .tabItem({
+                .tabItem {
                     Label("Featured", systemImage: "star")
-                })
+                }
                 .tag(Tab.featured)
+
             LandmarkList()
-                .tabItem({
+                .tabItem {
                     Label("List", systemImage: "list.bullet")
-                })
+                }
                 .tag(Tab.list)
         }
-        
     }
 }
 
