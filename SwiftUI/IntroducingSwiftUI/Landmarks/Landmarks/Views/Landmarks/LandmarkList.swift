5bbb086876d4c38d172dd40a253483d765052ebb
Javier Cruz Santiago <javiercruzsantiago@icloud.com>
2023-08-14 18:32:36 -0600

    [NEW]/[SwiftUI] - Added Create A macOS App chapter content for the Landmarks app

--- SwiftUI/IntroducingSwiftUI/Landmarks/Landmarks/Views/Landmarks/LandmarkList.swift
+++ SwiftUI/IntroducingSwiftUI/Landmarks/Landmarks/Views/Landmarks/LandmarkList.swift
@@ -3,27 +3,71 @@ import SwiftUI
 struct LandmarkList: View {
     @EnvironmentObject var modelData: ModelData
     @State private var showFavoritesOnly: Bool = false
+    @State private var filter: FilterCategory = .all
+    @State private var selectedLandmark: Landmark?
+    
+    enum FilterCategory: String, CaseIterable, Identifiable {
+        case all = "All"
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+        case mountains =  "Mountains"
+        
+        var id: FilterCategory { self }
+    }
     
     var filteredLandmarks: [Landmark] {
-        modelData.landmarks.filter { !showFavoritesOnly || $0.isFavorite }
+        modelData.landmarks.filter { landmark in
+            (
+                !showFavoritesOnly || landmark.isFavorite
+            ) && (
+                filter == .all || filter.rawValue == landmark.category.rawValue
+            )
+        }
+    }
+    
+    var title: String {
+        let title = filter == .all ? "Landmarks" : filter.rawValue
+        return showFavoritesOnly ? "Favorite \(title)" : title
+    }
+    
+    var index: Int? {
+        modelData.landmarks.firstIndex(where: { $0.id == selectedLandmark?.id } )
     }
     
     var body: some View {
         NavigationView {
-            List {
-                Toggle("Favorites Only", isOn: $showFavoritesOnly)
-                
+            List(selection: $selectedLandmark) {
                 ForEach(filteredLandmarks) { landmark in
                     NavigationLink {
                         LandmarkDetail(landmark: landmark)
                     } label: {
                         LandmarkRow(landmark: landmark)
                     }
-
+                    .tag(landmark)
                 }
-                .navigationTitle("Landmarks")
             }
+            .navigationTitle(title)
+            .frame(minWidth: 300)
+            .toolbar {
+                ToolbarItem {
+                    Menu {
+                        Picker("Category", selection: $filter) {
+                            ForEach(FilterCategory.allCases) { category in
+                                Text(category.rawValue).tag(category)
+                            }
+                        }
+                        .pickerStyle(.inline)
+                        
+                        Toggle("Favorites Only", isOn: $showFavoritesOnly)
+                    } label: {
+                        Label("Filter", systemImage: "slider.horizontal.3")
+                    }
+                }
+            }
+            
+            Text("Select a Landmark")
         }
+        .focusedValue(\.selectedLandmark, $modelData.landmarks[index ?? 0])
     }
 }
 
---
+++ SwiftUI/IntroducingSwiftUI/Landmarks/WatchLandmarks Watch App/LandmarkList.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly: Bool = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { !showFavoritesOnly || $0.isFavorite }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle("Favorites Only", isOn: $showFavoritesOnly)
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                    
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+            .environmentObject(ModelData())
+    }
+}
