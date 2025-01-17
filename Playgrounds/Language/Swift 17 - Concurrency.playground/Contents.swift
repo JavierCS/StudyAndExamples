b83ae74864ea17e3059477bf00a95f8e250b689f
Javier Cruz Santiago <javiercruzsantiago@icloud.com>
2025-01-17 14:00:59 -0600

    [DELETE] - Deleted md files, [UPDATE] - Modified Concurrency playground

--- Playgrounds/Language/Swift 17 - Concurrency.playground/Contents.swift
+++ Playgrounds/Language/Swift 17 - Concurrency.playground/Contents.swift
@@ -1,3 +1,28 @@
-import UIKit
+/*:
+# Concurrency
+*/
 
-var str = "Hello, playground"
+/**
+ Swift is a type-safe language, which means the language helps you to be clear about the types of values your code can work with.
+ */
+
+import Foundation
+
+// MARK: - Defining and Calling Asynchronous Functions
+
+func listPhotos(inGallery name: String) async throws -> [String] {
+    try await Task.sleep(for: .seconds(2))
+    return ["IMG001", "IMG99", "IMG0404"]
+}
+
+func nonThrowingListPhotos() async -> Result<[String], Error> {
+    do {
+        let photos = try await listPhotos(inGallery: "Test")
+        return .success(photos)
+    } catch {
+        return .failure(error)
+    }
+}
+
+let photos = try await listPhotos(inGallery: "Test")
+let nonThrowingPhotosResult = await nonThrowingListPhotos()
